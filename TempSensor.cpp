6275c486d390061ed20375d31285597ef5c55367
Rafał <rafal.mazerant@gmail.com>
2015-06-03 20:14:57 +0200

    Update TempSensor.cpp

--- TempSensor.cpp
+++ TempSensor.cpp
@@ -9,14 +9,20 @@ using namespace std;
 TempSensor::TempSensor(){
 	cout << "TempSensor default constructor" << endl;
 }
-TempSensor::TempSensor(SensorSystem* sys, string name, string link, double t_min, double t_max)
-	:Sensor(sys, name, link), t_min(t_min), t_max(t_max)
+TempSensor::TempSensor(SensorSystem* sys, string name, string link, double t_min, double t_max, double t_crit)
+	:Sensor(sys, name, link), t_min(t_min), t_max(t_max), t_crit(t_crit)
 {
 	cout << "TempSensor: " << name << " ," << link << "." << endl;
 	
 }
 TempSensor::~TempSensor(){
 }
-void TempSensor::measure(){
-	cout<<"Temperatura " <<name<<" : "<< ((rand() % (int)(t_max - t_min)) + t_min)<<"*C" << endl;
+void TempSensor::measure(SensorSystem* sys){
+	current_measure = ((rand() % (int)(t_max - t_min)) + t_min);
+	cout<<"Temperatura " <<name<<" : "<<current_measure<<"*C" << endl;
+	if (t_crit<=current_measure)
+	{
+		sys->CriticalValue(this);
+	};
+
 }
