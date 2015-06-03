54efbd267b6b3ca28f8a4f442e5ea4e619eeafa6
MrBruckner <piotr.m.jurek@gmail.com>
2015-06-03 20:35:38 +0200

    poprawa po Raph

--- System.h
+++ System.h
@@ -1,28 +1,21 @@
 #pragma once
 #include <iostream>
-#include <string>
+#include <vector>
 #include "Sensor.h"
-#include "System.h"
-#include "TempSensor.h"
 using namespace std;
 
-TempSensor::TempSensor(){
-	cout << "TempSensor default constructor" << endl;
-}
-TempSensor::TempSensor(SensorSystem* sys, string name, string link, double t_min, double t_max, double t_crit)
-	:Sensor(sys, name, link), t_min(t_min), t_max(t_max), t_crit(t_crit)
-{
-	cout << "TempSensor: " << name << " ," << link << "." << endl;
-	
-}
-TempSensor::~TempSensor(){
-}
-void TempSensor::measure(SensorSystem* sys){
-	current_measure = ((rand() % (int)(t_max - t_min)) + t_min);
-	cout<<"Temperatura " <<name<<" : "<<current_measure<<"*C" << endl;
-	if (t_crit<=current_measure)
-	{
-		sys->CriticalValue(this);
-	};
+class Sensor;
 
-}
+class SensorSystem
+{
+private:
+protected:
+	vector <Sensor*> sensors;
+public:
+	SensorSystem();
+	virtual ~SensorSystem();
+	void Register(Sensor* sensor);
+	void ShowAll();
+	void MeasureAll();
+	void CriticalValue(Sensor* sensor);
+};
