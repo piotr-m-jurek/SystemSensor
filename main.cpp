dff3b6522e3181eb96565d36e5857e8fbfe8922b
Rafał <rafal.mazerant@gmail.com>
2015-06-03 20:12:53 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -37,12 +37,14 @@ mutex.unlock()
 	srand(time(NULL));
 	SensorSystem* syst = new SensorSystem();
 	//dlaczego wywoluje sie domyslny konstruktor Sensor??
-	Sensor* newSensor = new TempSensor(syst, "w kuchni", "COM", +20.0, +30.0);
-	newSensor = new TempSensor(syst, "w kiblu", "UDP", +18.0, +35.0);
-	newSensor = new TempSensor(syst, "w pokoju", "Ethernet", +18.0, +21.0);
-	newSensor = new HumiditySensor(syst, "w pokoju", "Ethernet", +0.0, +120.0);
+	Sensor* newSensor = new TempSensor(syst, "w kuchni", "COM", +10.0, +30.0, +23.0);
+	newSensor = new HumiditySensor(syst, "w pokoju", "UDP", +0.0, +120.0);
+	syst->ShowAll();
+	//newSensor = new TempSensor(syst, "w kiblu", "UDP", +18.0, +35.0);
+	//newSensor = new TempSensor(syst, "w pokoju", "Ethernet", +18.0, +21.0);
 	syst->MeasureAll();
 	//syst->ShowAll();
+
 	//delete syst;
 	_getch();
 	return 0;
